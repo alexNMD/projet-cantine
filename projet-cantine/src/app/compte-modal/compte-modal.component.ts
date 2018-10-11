2a2cc5b503685f6b5dd317c8cc25ea29afa45afe
Ju314ter <julien.feger@gmail.com>
2018-10-11 11:48:01 +0200

    2nd modal commit

--- projet-cantine/src/app/compte-modal/compte-modal.component.ts
+++ projet-cantine/src/app/compte-modal/compte-modal.component.ts
@@ -8,6 +8,8 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CompteModalComponent implements OnInit {
 
+  user: string = "Julien";
+
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
