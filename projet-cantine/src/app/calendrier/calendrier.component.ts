043112fbacd5c2846bba9b3df62239e5266945ab
Alexandre Normand <ale.normand@icloud.com>
2018-11-03 19:25:22 +0100

    séparation du code (templateUrl)

--- projet-cantine/src/app/calendrier/calendrier.component.ts
+++ projet-cantine/src/app/calendrier/calendrier.component.ts
@@ -2,17 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calendrier',
-  template: `
-  <div *ngFor="let day of days">
-        <ul class="list-group">
-          <li class="text-center list-group-item li-title">
-              {{ day }}
-          </li>
-          <app-plat></app-plat>
-        </ul>
-  </div>
-      `,
-  /*templateUrl: './calendrier.component.html',*/
+  templateUrl: './calendrier.component.html',
   styleUrls: ['./calendrier.component.css']
 })
 export class CalendrierComponent implements OnInit {
--- projet-cantine/src/app/plat/plat.component.ts
+++ projet-cantine/src/app/plat/plat.component.ts
@@ -4,16 +4,7 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-plat',
-  template: `
-      <li *ngFor="let plat of plats" class="list-group-item d-flex justify-content-between">
-        <span>{{ plat }}</span>
-        <div>
-          <button type="button" class="btn btn-primary btn-sm mr-1" (click)="openFormModalPlat()"><i class="fas fa-info"></i></button>
-          <button type="button" class="btn btn-success btn-sm" (click)="addFoodItem()"><i class="fas fa-plus"></i></button>
-        </div>
-      </li>
-      `,
-  /*templateUrl: './plat.component.html',*/
+  templateUrl: './plat.component.html',
   styleUrls: ['./plat.component.css']
 })
 export class PlatComponent implements OnInit {
