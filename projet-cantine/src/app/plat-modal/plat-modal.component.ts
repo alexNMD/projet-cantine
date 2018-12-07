1a2a823517c57f2f290bf5f1bd905af3bc21f387
Mendelrock <lamiethno@gmail.com>
2018-12-07 16:01:05 +0100

    MENU PUTAIN

--- projet-cantine/src/app/plat-modal/plat-modal.component.ts
+++ projet-cantine/src/app/plat-modal/plat-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Plat } from '../plat';
@@ -9,14 +9,19 @@ import { Plat } from '../plat';
   styleUrls: ['./plat-modal.component.css']
 })
 export class PlatModalComponent implements OnInit {
-
-  plat: Plat;
+  @Input() name: string;
+  @Input() price: number;
+  @Input() temp: string;
+  @Input() type_dish: string;
+  @Input() image: string;
+  @Input() ingredients: Array<string>;
+  @Input() date: Date;
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
+    console.log(this.ingredients);
   }
-
-    closeModal() {
+  closeModal() {
         this.activeModal.close('Modal closed');
     }
 }
